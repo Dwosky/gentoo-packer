0d217f21d12d8b1e9bc5fe5ad771c6c03503d771
Peter <dwosky@zoho.com>
2018-11-21 17:04:48 +0100

    Moved binding steps to 04

--- scripts/02_mount.sh
+++ scripts/02_mount.sh
@@ -4,10 +4,3 @@ echo "==> Mounting the created filesystems"
 mount ${DISK}4 ${BASE_DIR}
 mkdir ${BASE_DIR}/boot
 mount ${DISK}1 ${BASE_DIR}/boot
-
-echo "==> Binding the needed filesystems"
-mount --types proc /proc ${BASE_DIR}/proc
-mount --rbind /sys ${BASE_DIR}/sys
-mount --make-rslave ${BASE_DIR}/sys
-mount --rbind /dev ${BASE_DIR}/dev
-mount --make-rslave ${BASE_DIR}/dev 
