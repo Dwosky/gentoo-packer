526907f1d6bba874306754cfa296f6d08d29011c
Peter <dwosky@zoho.com>
2018-11-21 17:05:55 +0100

    Added step 07, system configuration

---
+++ scripts/07_kernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==> Emerging the latest kernel sources & genkernel"
+
+echo "sys-apps/util-linux static-libs" >> ${BASE_DIR}/etc/portage/package.use/genkernel
+chroot ${BASE_DIR} /bin/bash << 'EOF'
+    emerge -q sys-kernel/gentoo-sources sys-kernel/genkernel
+    eselect kernel list
+    eselect kernel set 1
+EOF
+
+echo "==> Configure and build the kernel with genkernel"
+chroot ${BASE_DIR} /bin/bash << 'EOF'
+    genkernel --install --symlink --no-dmraid --no-zfs --no-btrfs --no-nfs all
+EOF
