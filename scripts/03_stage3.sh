96b306b076e4f4031f5b1f175efc5ca5d4f0e35c
Peter <dwosky@zoho.com>
2018-11-21 17:05:25 +0100

    Added quiet flag to wget
    In order to try to clean the packer log

--- scripts/03_stage3.sh
+++ scripts/03_stage3.sh
@@ -2,7 +2,7 @@
 
 echo "==> Downloading the Stage3 tarball to ${BASE_DIR}"
 cd ${BASE_DIR}
-wget ${MIRROR_URL}/${TARBALL}
+wget -q ${MIRROR_URL}/${TARBALL}
 echo "==> Unpacking the tarball ${TARBALL}"
 tar xpf ${TARBALL} --xattrs-include='*.*' --numeric-owner
 echo "==> Removing the tarball ${TARBALL}"
