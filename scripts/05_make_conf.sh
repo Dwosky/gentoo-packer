136b7db70ae3bfafd2b08139427d9a3e58276be0
Pedro Arizmendi <dwosky@zoho.com>
2019-10-08 16:15:57 +0200

    Added repo conf

--- scripts/05_make_conf.sh
+++ scripts/05_make_conf.sh
@@ -9,3 +9,12 @@ echo "MAKEOPS=\"${MAKE_OPS}\"" >> $MAKE_CONF
 
 echo "==> Selecting the best mirrors in $MAKE_CONF"
 mirrorselect -s3 -R ${REGION} -o >> $MAKE_CONF
+
+echo "==> Setup ebuild repository"
+mkdir --parents ${BASE_DIR}/etc/portage/repos.conf
+echo "[DEFAULT]" >> ${BASE_DIR}/etc/portage/repos.conf/webrsync.conf
+echo "main-repo = gentoo" >> ${BASE_DIR}/etc/portage/repos.conf/webrsync.conf
+echo "[gentoo]" >> ${BASE_DIR}/etc/portage/repos.conf/webrsync.conf
+echo "location = /var/db/repos/gentoo" >> ${BASE_DIR}/etc/portage/repos.conf/webrsync.conf
+echo "sync-type = webrsync" >> ${BASE_DIR}/etc/portage/repos.conf/webrsync.conf
+echo "auto-sync = yes" >> ${BASE_DIR}/etc/portage/repos.conf/webrsync.conf
