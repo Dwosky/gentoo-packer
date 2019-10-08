cbaa2045b52995559d70ed8bff2a1e62a485ad8e
Pedro Arizmendi <dwosky@zoho.com>
2019-10-08 16:16:34 +0200

    Added use flag & license

--- scripts/07_kernel.sh
+++ scripts/07_kernel.sh
@@ -3,10 +3,10 @@
 echo "==> Emerging the latest kernel sources & genkernel"
 
 echo "sys-apps/util-linux static-libs" >> ${BASE_DIR}/etc/portage/package.use/genkernel
+echo "sys-kernel/gentoo-sources symlink" >> ${BASE_DIR}/etc/portage/package.use/genkernel
+echo "sys-kernel/linux-firmware linux-fw-redistributable no-source-code" >> ${BASE_DIR}/etc/portage/package.license
 chroot ${BASE_DIR} /bin/bash << 'EOF'
     emerge -q sys-kernel/gentoo-sources sys-kernel/genkernel
-    eselect kernel list
-    eselect kernel set 1
 EOF
 
 echo "==> Configure and build the kernel with genkernel"
