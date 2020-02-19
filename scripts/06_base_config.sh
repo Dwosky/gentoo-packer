25811c350395ddc5278dd864a8a868cf8cc37c26
Peter <dwosky@pm.me>
2020-02-19 22:56:49 +0100

    Moved /boot partition below / to avoid mounting issues

--- scripts/06_base_config.sh
+++ scripts/06_base_config.sh
@@ -19,6 +19,6 @@ EOF
 
 echo "==> Setting /etc/fstab entries"
 echo "# <fs>    <mount>	<type>	<opts>		<dump/pass>" >> $BASE_DIR/etc/fstab
-echo "/dev/sda1	/boot	ext2	noauto,noatime	1 2" >> $BASE_DIR/etc/fstab
 echo "/dev/sda4	/	    ext4	noatime		    0 1" >> $BASE_DIR/etc/fstab
+echo "/dev/sda1	/boot	ext2	noauto,noatime	1 2" >> $BASE_DIR/etc/fstab
 echo "/dev/sda3	none	swap	sw		        0 0" >> $BASE_DIR/etc/fstab
