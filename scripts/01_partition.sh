7130f89a849d645d29d524b6f44d5b67300b600a
Peter <dwosky@pm.me>
2020-02-19 22:55:40 +0100

    Updated msg to reflect partition number

--- scripts/01_partition.sh
+++ scripts/01_partition.sh
@@ -3,16 +3,16 @@
 echo "==> Create GPT partition table on ${DISK}"
 sgdisk -og ${DISK}
 
-echo "==> Creating /boot partition on ${DISK}"
+echo "==> Creating /boot partition on ${DISK}1"
 sgdisk -n 1:0:+128M -c 1:"Linux boot partition" -t 1:8300 ${DISK}
 
-echo "==> Creating GRUB partition on ${DISK}"
+echo "==> Creating GRUB partition on ${DISK}2"
 sgdisk -n 2:0:+32M -c 2:"Linux GRUB partition" -t 2:ef02 -A 2:set:2 ${DISK}
 
-echo "==> Creating swap partition on ${DISK}"
+echo "==> Creating swap partition on ${DISK}3"
 sgdisk -n 3:0:+1G -c 3:"Linux swap partition" -t 3:8200 ${DISK}
 
-echo "==> Creating /root partition on ${DISK}"
+echo "==> Creating /root partition on ${DISK}4"
 sgdisk -n 4:0:0 -c 4:"Linux root partition" -t 4:8300 ${DISK}
 
 echo "==> Formating the partitions on ${DISK}"
